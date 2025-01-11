c40ee808d4707331a84acea7018e2fabfa43b459
AnabelenScript <ana0507belen@gmail.com>
2025-01-11 15:51:01 -0600

    Solucion de logica de colisiones

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,28 @@ import GamePage from './components/GamePage'
 
 function App() {
   return (
-    <GamePage></GamePage>
+    <>
+    <div className='main'>
+    <GamePage></GamePage></div>
+    <div className='Mainbackground'>
+    <div id="controls">
+        <span className="control-item">
+          <span className="key">Space</span> Jugar/Restart
+        </span>
+        <span className="control-item">
+          <span className="key">M</span> Silencio
+        </span>
+        <span className="control-item">
+          <span className="key pixel-icon"><i className='bx bx-chevron-left'></i></span>
+          <span className="key pixel-icon"><i className='bx bx-chevron-right'></i></span> Mover
+        </span>
+        <span className="control-item">
+          <span className="key pixel-icon"><i className='bx bx-chevron-up'></i></span>
+          <span className="key pixel-icon"><i className='bx bx-chevron-down'></i></span> Mover
+        </span>
+      </div>
+
+      </div></>
   )
 }
 
